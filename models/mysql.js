92fdb017e10d4f04b3a619e1fc57ce2e198d7274
Shane Chang <kxykxyou@gmail.com>
2022-11-04 16:50:25 +0800

    feat: remove original mysql pool which will be not used

--- models/mysql.js
+++ models/mysql.js
@@ -10,11 +10,4 @@ const db = mysql
   })
   .promise()
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
-
-module.exports = { db, pool }
+module.exports = { db }
