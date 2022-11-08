48dab347a4c78323f20e030cd5bb512c82cecf78
Shane Chang <kxykxyou@gmail.com>
2022-11-08 17:36:04 +0800

    api: GET /inpatients/charged provide multiple fields

--- models/inpatientsModel.js
+++ models/inpatientsModel.js
@@ -11,7 +11,38 @@ const inpatientQueryFields = [
 ]
 
 async function getChargedPets () {
-  const [data] = await db.query('SELECT * FROM cage')
+  const sql = `
+    SELECT *
+    FROM
+    (
+      SELECT 
+      i.id as inpatientId,
+      i.code as inpatientCode, 
+      i.charge_start as chargeStart,
+      i.charge_end as chargeEnd,
+      i.cage as cage,
+      i.summary as summary,
+      p.id as petId,
+      p.name as petName, 
+      p.code as petCode,
+      p.chip as petChip,
+      p.status as petStatus,
+      b.species as petSpecies,
+      b.breed as petBreed,
+      o.fullname as ownerFullname,
+      o.cellphone as ownerCellphone,
+      u.id as vetId,
+      u.fullname as vetFullname
+      FROM inpatient as i
+      JOIN user as u on i.vet_id = u.id
+      JOIN pet as p on i.pet_id = p.id 
+      JOIN breed as b on p.breed_id = b.id
+      JOIN owner as o on p.owner_id = o.id 
+    ) AS new_table
+    WHERE chargeEnd IS NULL
+    ORDER BY cage
+  `
+  const [data] = await db.query(sql)
   return data
 }
 
