dc7dffb230bdf2dae41fe33d9c213cd4d82fd990
Shane Chang <kxykxyou@gmail.com>
2022-11-08 14:50:25 +0800

    api: serve GET /inpatients/charged

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use('/api/' + process.env.API_VERSION, [
   require('./routes/records'),
   require('./routes/breeds'),
   require('./routes/vets'),
-  require('./routes/pets')
+  require('./routes/pets'),
+  require('./routes/inpatients')
 
 ])
 
