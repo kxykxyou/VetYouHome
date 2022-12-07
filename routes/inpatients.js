b1fbef0850580308e9d2e72065f44786e332c2d9
Shane Chang <kxykxyou@gmail.com>
2022-12-07 15:49:05 +0800

    refactor: clinic routes to RESTful style and add checkId if params conains /:id

--- routes/inpatients.js
+++ routes/inpatients.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { wrapAsync } = require('../utils/utils')
+const { wrapAsync, checkId } = require('../utils/utils')
 const inpatientsController = require('../controllers/inpatientsController')
 
 const router = express.Router()
@@ -8,9 +8,9 @@ const router = express.Router()
 /* Routes. */
 router.get('/inpatients/charged', wrapAsync(inpatientsController.getChargedPets))
 router.get('/inpatients/search', wrapAsync(inpatientsController.searchInpatients))
-router.get('/inpatients/id/:id/inpatientorders/complex/today', wrapAsync(inpatientsController.getTodayInpatientOrderComplexByInpatientId))
+router.get('/inpatients/id/:id/inpatientorders/complex/today', checkId, wrapAsync(inpatientsController.getTodayInpatientOrderComplexByInpatientId))
 
-router.get('/inpatientorders/complex/id/:id', wrapAsync(inpatientsController.getInpatientOrderComplexByInpatientOrderId))
+router.get('/inpatientorders/complex/id/:id', checkId, wrapAsync(inpatientsController.getInpatientOrderComplexByInpatientOrderId))
 
 router.post('/inpatients/discharge', wrapAsync(inpatientsController.discharge))
 router.post('/inpatients/swapcage', wrapAsync(inpatientsController.swapCage))
